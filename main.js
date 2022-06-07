eebbfe327d17bd04c4d99930df335c0e600c0a1e
Shreya <87652405+ShreyaSudheer@users.noreply.github.com>
2022-06-07 15:41:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function dress()
+{
+    window.location = "dresses.html";
+}
+function back()
+{
+    window.location = "index.html";
+}
+function book()
+{
+    window.location = "book.html";
+}
+function fbasket()
+{
+    window.location = "fbasket.html";
+}
+function bottle()
+{
+    window.location = "bottles.html";
+}
+function shoes()
+{
+    window.location = "shoes.html";
+}
